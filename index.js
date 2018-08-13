82eaad5f4fb57d796acb99b2cf1616c6d8d73092
Melnyk Eugene <melnyk.eugene.o@gmail.com>
2018-08-12 22:12:23 -0400

    done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http=require('http');
 var fs = require('fs');
 var path = require('path');
-var mime = require('mime-types');
+var mime = require('mime');
 var cache={};
 
 function send404(response){
@@ -55,4 +55,10 @@ var server=http.createServer(function(request,response){
 server.listen(3000,function(){
 	console.log("Server listening on port 3000.");
 });
+
+var chatServer = require('./lib/chat_server');
+console.log('Attempt to load socketio server');
+chatServer.listen(server);
+
+
 
